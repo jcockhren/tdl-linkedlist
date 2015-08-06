a80d6b06d170a009f1ac51c73580e1b51f8b51e6
Jurnell Cockhren <jurnell.cockhren@sophicware.com>
2015-08-05 19:31:29 -0700

    first implementation of ToString

--- SinglyLinkedLists/SinglyLinkedList.cs
+++ SinglyLinkedLists/SinglyLinkedList.cs
@@ -163,5 +163,31 @@ namespace SinglyLinkedLists
         {
             throw new NotImplementedException();
         }
+
+        public override string ToString()
+        {
+            var opening = "{";
+            var ending = "}";
+            var space = " ";
+            var output = "";
+            var quote = "\"";
+            var comma = "," + space;
+            output += opening;
+            var node = this.first_node;
+            if (this.Count() >= 1)
+            {
+                output += space;
+                while (!node.IsLast())
+                {
+                    output += quote + node.Value + quote + comma;
+                    node = node.Next;
+                }
+                output += quote + this.Last() + quote;
+            }
+            output += space;
+            output += ending;
+            return output;
+        }
+
     }
 }
